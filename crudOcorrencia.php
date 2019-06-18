a0ac45f63157946da32957cfbce3d7d76e422ca2
angmerlo <51007877+angmerlo@users.noreply.github.com>
2019-06-18 20:21:37 -0300

    Ajuste no mensagem de erros, para ficar responsivo

--- crudOcorrencia.php
+++ crudOcorrencia.php
@@ -44,7 +44,7 @@ if($acao == "index"){
       }else if($acao == "novo"){
     $listaOcorrencias = $daoOcorrencia->buscarOcorrencias();
     require './cabecalho.php';
-    require './erros.php';
     require './formOcorrencia.php';
     require './rodape.php';
+    require './erros.php';
 } 
